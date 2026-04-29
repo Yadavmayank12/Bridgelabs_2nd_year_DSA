adf1cc0f3296d81edad215cfecbd3147ac9fa314
Yadavmayank12 <149813224+Yadavmayank12@users.noreply.github.com>
2026-04-30 02:26:21 +0530

    Added by Mayank

---
+++ Stacks/MyMap.java
@@ -0,0 +1,70 @@
+public class MyMap {
+
+    static class Node {
+        int k, v;
+        Node next;
+
+        Node(int k, int v) {
+            this.k = k;
+            this.v = v;
+        }
+    }
+
+    Node[] b = new Node[100];
+
+    void put(int k, int v) {
+        int i = k % 100;
+        Node cur = b[i];
+
+        while (cur != null) {
+            if (cur.k == k) {
+                cur.v = v;
+                return;
+            }
+            cur = cur.next;
+        }
+
+        Node n = new Node(k, v);
+        n.next = b[i];
+        b[i] = n;
+    }
+
+    int get(int k) {
+        int i = k % 100;
+        Node cur = b[i];
+
+        while (cur != null) {
+            if (cur.k == k)
+                return cur.v;
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    void remove(int k) {
+        int i = k % 100;
+        Node cur = b[i], prev = null;
+
+        while (cur != null) {
+            if (cur.k == k) {
+                if (prev == null)
+                    b[i] = cur.next;
+                else
+                    prev.next = cur.next;
+                return;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyMap map = new MyMap();
+        map.put(1, 100);
+        map.put(2, 200);
+
+        System.out.println(map.get(1));
+        map.remove(1);
+        System.out.println(map.get(1));
+    }
+}
---
+++ Stacks/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int a[] = {2, 7, 11, 15}, t = 9;
+
+        HashMap<Integer, Integer> m = new HashMap<>();
+
+        for (int i = 0; i < a.length; i++) {
+            if (m.containsKey(t - a[i])) {
+                System.out.println(m.get(t - a[i]) + " " + i);
+                return;
+            }
+            m.put(a[i], i);
+        }
+    }
+}
